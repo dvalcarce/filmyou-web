de4328332af9388428cf0857ed77a60142320d16
dvalcarce <danivalsil@gmail.com>
2014-04-19 12:46:50 +0200

    Write settings following the "One True Way"

---
+++ src/filmyou/settings/test.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+from .base import *
+
+########## TEST SETTINGS
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
+TEST_DISCOVER_ROOT = SITE_ROOT
+TEST_DISCOVER_PATTERN = "test_*.py"
+########## IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
