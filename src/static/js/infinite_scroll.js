eaff62f15c156ffa1e49c4fedfafc94633fe3d4a
dvalcarce <danivalsil@gmail.com>
2014-04-19 12:21:45 +0200

    Update to Boostrap 3.1.1 and JQuery 2.1.0

--- src/static/js/infinite_scroll.js
+++ src/static/js/infinite_scroll.js
@@ -1,22 +1,22 @@
 (function ($) {
     'use strict';
 
-    $.fn.endlessPaginate = function(options) {
+    $.fn.endlessPaginate = function (options) {
         var settings = {
             // Twitter-style pagination container selector.
             containerSelector: '#scrolling_container',
             loadingSelector: '#scrolling_loading',
             moreSelector: '#more_results',
             // If paginate-on-scroll is on, this margin will be used.
-            magin : 20,
+            magin: 20,
         }
 
-        return this.each(function() {
+        return this.each(function () {
             var element = $(this),
                 loadedPages = 1;
 
             // Twitter-style pagination.
-            element.on('click', settings.moreSelector, function() {
+            element.on('click', settings.moreSelector, function () {
                 var container = link.closest(settings.containerSelector),
                     loading = $(loadingSelector);
                 // Avoid multiple Ajax calls.
@@ -26,7 +26,7 @@
                 loading.show();
                 var data = 'querystring_key=' + link.attr('rel').split(' ')[0];
                 // Send the Ajax request.
-                $.get(link.link.attr('href'), data, function(fragment) {
+                $.get(link.link.attr('href'), data, function (fragment) {
                     container.before(fragment);
                     container.remove();
                     // Increase the number of loaded pages.
@@ -38,18 +38,21 @@
             // On scroll pagination.
             var win = $(window),
                 doc = $(document);
-            win.scroll(function(){
+            win.scroll(function () {
                 if (doc.height() - win.height() - win.scrollTop()
                     <= settings.magin) {
-                        element.find(settings.moreSelector).click();
-                    }
+                    element.find(settings.moreSelector).click();
                 }
-            });
+            }
         });
-    };
+    }
+    )
+    ;
+};
 
-    $.endlessPaginate = function(options) {
-        return $('body').endlessPaginate(options);
-    };
+$.endlessPaginate = function (options) {
+    return $('body').endlessPaginate(options);
+};
 
-})(jQuery);
+})
+(jQuery);
