60317dda9795391dd6468b573f5e1038ae1fe384
dvalcarce <danivalsil@gmail.com>
2014-05-26 02:12:37 +0200

    Optimize DB access: use of prefetch_related()

--- src/apps/utils/db.py
+++ src/apps/utils/db.py
@@ -3,17 +3,20 @@
 from __future__ import absolute_import
 
 
-def retrieve_in_order_from_db(model, ids):
+def retrieve_in_order_from_db(model, ids, prefetch=True):
     """
     Retrieve entities of the given model from the RDBMS in order given their ids.
     :param model: model of the entities
     :param ids: ids of the entities
+    :param prefetch: prefetch many-to-many relationships
     :return: a list of entities
     """
-    # Retrieve from RDBMS
-    entities = model.objects.in_bulk(ids)
-
-    #TODO: prefetch_related
+    # Prefetch related
+    if prefetch:
+        relationships = [m2m.attname for m2m in model._meta._many_to_many()]
+        entities = model.objects.all().prefetch_related(*relationships).in_bulk(ids)
+    else:
+        entities = model.objects.in_bulk(ids)
 
     # Order by search order
     ordered_entities = [entities.get(id, None) for id in ids]
