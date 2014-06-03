13336d3152e6d080a15529c3e555a4de900e45f0
dvalcarce <danivalsil@gmail.com>
2014-06-03 23:30:51 +0200

    Add static urls to urls.py

--- src/filmyou/urls.py
+++ src/filmyou/urls.py
@@ -30,4 +30,6 @@ urlpatterns = patterns(
     url(r'^autocomplete/', include('autocomplete_light.urls')),
 )
 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
